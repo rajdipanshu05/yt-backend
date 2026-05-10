0cdf62a302dfc8bccfb97bdd0a78ba53f1ff63ea
Dipanshu Raj <rajdipanshu180@gmail.com>
2026-05-11 01:44:27 +0530

    user controller

--- src/db/index.js
+++ src/db/index.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
 const connectDB = async ()=>{
-    console.log(process.env.MONGODB_URL);
+    // console.log(process.env.MONGODB_URL);
     try{
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
         console.log(`\n MongoDB Connected !! DB HOST : ${connectionInstance.connection.host}`);
